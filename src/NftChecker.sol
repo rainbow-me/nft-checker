73a9730871193c175b8e8d7b445a0ddaa29f4f91
Daniel Sinclair <d@niel.nyc>
2024-11-12 17:35:35 -0500

    fix: tuple type overload for token ids

--- src/NftChecker.sol
+++ src/NftChecker.sol
@@ -9,7 +9,8 @@ import {IERC1155} from "src/interfaces/IERC1155.sol";
 /// @author asnared <https://github.com/abigger87>
 /// @notice Checks if one of many NFTs are ownerd by any of the given addresses
 contract NftChecker {
-    /// @notice Returns if one of the given addresses is a token owner.
+
+    /// @notice Returns if one of the given addresses is a token owner for ERC-721 tokens.
     function areOwners(address[] memory targets, address[] memory potentialOwners) public view returns (bool) {
         uint256 targetsLength = targets.length;
         uint256 potentialOwnersLength = potentialOwners.length;
@@ -23,8 +24,35 @@ contract NftChecker {
                 if (IERC721(targets[i]).balanceOf(potentialOwners[j]) > 0) {
                     return true;
                 }
+                unchecked {
+                    ++j;
+                }
+            }
+            unchecked {
+                ++i;
+            }
+        }
+        return false;
+    }
+
+    /// @notice Struct to hold an NFT address and token ID
+    struct TokenInfo {
+        address account;
+        uint256 id;
+    }
+
+    /// @notice Returns if one of the given addresses is a token owner for ERC-1155 tokens.
+    function areOwners(TokenInfo[] memory targets, address[] memory potentialOwners) public view returns (bool) {
+        uint256 targetsLength = targets.length;
+        uint256 potentialOwnersLength = potentialOwners.length;
+        uint256 i;
+        uint256 j;
+        // for every contract in the targets array
+        for (i = 0; i < targetsLength;) {
+            // for every address in the potential owners array
+            for (j = 0; j < potentialOwnersLength;) {
                 // if the potential owner is an actual owner of ERC1155
-                if (IERC1155(targets[i]).balanceOf(potentialOwners[j], 1) > 0) { // Assuming token ID 1 for ERC1155
+                if (IERC1155(targets[i].account).balanceOf(potentialOwners[j], targets[i].id) > 0) {
                     return true;
                 }
                 unchecked {
